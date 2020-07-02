15dcdd71624fbdec6b625b9ba86f58023ccc5477
Andy Alexis <andy_mar_rey@outlook.com>
2020-07-01 19:06:30 -0500

    cambios open graph

--- public/_head.php
+++ public/_head.php
@@ -14,24 +14,24 @@ $imgWeb = "assets/img/banner.jpg";
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<meta name="description" content=<?php echo $descripccionWeb; ?>>
-	<link rel="shortcut icon" href=<?php echo $favicon; ?>>
+	<meta name="description" content="<?php echo $descripccionWeb; ?>">
+	<link rel="shortcut icon" href="<?php echo $favicon; ?>">
 	<!-- OPEN GRAPH -->
-	<meta property="og:title" content=<?php echo $tituloWebRedes; ?> />
+	<meta property="og:title" content="<?php echo $tituloWebRedes; ?>" />
 	<meta property="og:type" content="website" />
-	<meta property="og:url" content=<?php echo $urlWeb; ?> />
-	<meta property="og:image" content=<?php echo $imgWeb; ?> />
-	<meta property="og:description" content=<?php echo $descripccionRedes; ?> />
+	<meta property="og:url" content="<?php echo $urlWeb; ?>" />
+	<meta property="og:image" content="<?php echo $imgWeb; ?>" />
+	<meta property="og:description" content="<?php echo $descripccionRedes; ?>" />
 	<!-- Schema.org markup for Google+ -->
-	<meta itemprop="name" content=<?php echo $tituloWebRedes; ?>>
-	<meta itemprop="description" content=<?php echo $descripccionRedes; ?>>
-	<meta itemprop="image" content=<?php echo $imgWeb; ?>>
+	<meta itemprop="name" content="<?php echo $tituloWebRedes; ?>">
+	<meta itemprop="description" content="<?php echo $descripccionRedes; ?>">
+	<meta itemprop="image" content="<?php echo $imgWeb; ?>">
 	<!-- Twitter Card data -->
-	<meta name="twitter:card" content=<?php echo $twitterCard; ?>>
-	<meta name="twitter:site" content=<?php echo $urlWeb; ?>>
-	<meta name="twitter:title" content=<?php echo $tituloWebRedes; ?>>
-	<meta name="twitter:description" content=<?php echo $descripccionWeb; ?>>
-	<meta name="twitter:image:src" content=<?php echo $imgWeb; ?>>
+	<meta name="twitter:card" content="<?php echo $twitterCard; ?>">
+	<meta name="twitter:site" content="<?php echo $urlWeb; ?>">
+	<meta name="twitter:title" content="<?php echo $tituloWebRedes; ?>">
+	<meta name="twitter:description" content="<?php echo $descripccionWeb; ?>">
+	<meta name="twitter:image:src" content="<?php echo $imgWeb; ?>">
 	<!-- FONTAWESOME -->
 	<link rel="stylesheet" href="assets/fontawesome/css/all.css">
 	<script src="assets/fontawesome/js/all.js"></script>
